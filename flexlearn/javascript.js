1026a820f74919559c91d40db06287d2f9503925
nirjalaM6 <141786545+nirjalaM6@users.noreply.github.com>
2024-04-11 23:30:08 -0400

    flexbox in css

---
+++ flexlearn/javascript.js
@@ -0,0 +1,6 @@
+// alert("Hi Nirjala, welcomr to web development");
+console.log("I have reached to 1");
+console.log("I have reached to 2");
+alert("Hi Nirjala, welcomr to web development");
+console.log("I have reached to 3");
+
